1b9d27855ec787b14557206fe5f62bd9b5637588
BANYAN <jesusblessher@gmail.com>
2020-05-20 23:19:01 +0800

    RxSwift MVVM .xctemplate

---
+++ Xcode-Project-Templates/RxSwift MVVM.xctemplate/UICollectionViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,35 @@
+//___FILEHEADER___
+
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    // MARK: - UI
+    
+    
+    // MARK: - ViewModel
+    
+    
+    // MARK: - Public Methods
+    
+    
+    // MARK: - Private Methods
+    
+    
+    // MARK: - Getter
+    
+}
